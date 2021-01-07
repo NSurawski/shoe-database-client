8509b4ee7d06329c6248ee4b48551eebd124f71d
nicolesurawski <nicolesurawski@gmail.com>
2021-01-07 13:40:36 -0500

    Added .js files in scripts folder, added basix html forms, added curl-script files for client side

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,14 @@
 'use strict'
-
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const userEvents = require('./shoes/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
+$('#signUp').on('submit', userEvents.onSignUp)
+$('#signIn').on('submit', userEvents.onSignIn)
+$('#changePassword').on('submit', userEvents.onChangePass)
+$('#signOutBtn').on('click', userEvents.onSignOut)
 })
